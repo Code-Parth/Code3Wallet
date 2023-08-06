508da072cab3d24e387c4feea96f93c705d4f3ac
Parth Parmar <84669955+Code-Parth@users.noreply.github.com>
2023-08-07 01:04:08 +0530

    Update chains.js

--- extension/src/chains.js
+++ extension/src/chains.js
@@ -2,7 +2,7 @@
 const Ethereum = {
     hex: '0x1', // The hexadecimal representation of the chain identifier
     name: 'Ethereum', // The human-readable name of the chain
-    rpcUrl: '', // The RPC (Remote Procedure Call) URL used to interact with the chain (to be filled later)
+    rpcUrl: 'https://mainnet.infura.io/v3/', // The RPC (Remote Procedure Call) URL used to interact with the chain
     ticker: "ETH" // The ticker symbol representing the native currency of the chain
 };
 
@@ -10,7 +10,7 @@ const Ethereum = {
 const MumbaiTestnet = {
     hex: '0x13881', // The hexadecimal representation of the chain identifier
     name: 'Mumbai Testnet', // The human-readable name of the chain
-    rpcUrl: '', // The RPC (Remote Procedure Call) URL used to interact with the chain (to be filled later)
+    rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/BNup1-Ogho6GsLllHnRHt2fzC-MEJfpN', // The RPC (Remote Procedure Call) URL used to interact with the chain (to be filled later)
     ticker: "MATIC" // The ticker symbol representing the native currency of the chain
 };
 
